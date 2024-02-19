e0b71dfdbe1533b6f3ae62504d191c89a9d5492c
reedclanton <clantonreed@gmail.com>
2024-02-19 15:51:50 -0600

    Fixed Sensors
    Turns out the library that's suposed to be used with the sensor,
    according to the seller/man, doesn't work. Used another library.

--- arduino-temperature-sensor/arduino-temperature-sensor.ino
+++ arduino-temperature-sensor/arduino-temperature-sensor.ino
@@ -1,47 +1,72 @@
 /** External Import(s) **/
-#include "Arduino_SensorKit.h"
+#include <Adafruit_AHTX0.h>
+#include <Arduino_SensorKit.h>
 
 /** Define Macro(s) **/
 /* Assign Pin(s) */
 // NoOp
-/* Set What Hardware is Used */
-#define USE_OLED true
-#define USE_SERIAL true
-#define USE_PRESSURE_SENSOR true
 /* Configuration Value(s) */
-#define LOOP_WAIT 0
+#define LOOP_WAIT 1000
+#define SERIAL_BAUD 115200
 /* Default Value(s) */
 // NoOp
 
-/** Custom Import(s) **/
-#include "output.h"
-#include "math.h"
+/** Local Import(s) **/
+// NoOp
 
 /** Global Variable(s) **/
-// NoOp
+Adafruit_AHTX0 aht;
 
 
 void setup() {
-  /* Output Setup */
-  outputSetup(USE_OLED, USE_SERIAL);
+  // Serial setup.
+  Serial.begin(SERIAL_BAUD);
 
-  /* Input Setup */
-  // NoOp
+  // Temperature & humidity setup.
+  if (! aht.begin()) {
+    Serial.println("#*# Could not find AHT? Check wiring...");
+	while (1) delay(10);
+  }
+  Serial.println("#*# AHT10 or AHT20 found.");
 
-  /* Sensor Setup */
-  if (USE_PRESSURE_SENSOR) { Pressure.begin(); }
+  // Pressure, temperature, & altitude setup.
+  if (! Pressure.begin()) {
+    Serial.println("#*# Could not initialize pressure sensor...");
+	while (1) delay(10);
+  }
+  Serial.println("#*# Pressure sensor initialized.");
 }
 
 void loop() {
-  /* Pressure Sensor */
-  if (USE_PRESSURE_SENSOR) {
-    outputAdd("Temp:       ", String(CelsiusToFahrenheit(Pressure.readTemperature())), " F");
-    outputAdd("Pres:   ", String(Pressure.readPressure()), " Pa");
-    outputAdd("Alt:   ", String(Pressure.readAltitude()), " m");
+  // Get sensor data from first sensor set.
+  sensors_event_t ahtx0_humidity, ahtx0_temp;
+  aht.getEvent(&ahtx0_humidity, &ahtx0_temp);
+  // Get sensor data from second sensor set.
+  float bmp280_temp = Pressure.readTemperature();
+  uint32_t bmp280_pressure = Pressure.readPressure();
+  float bmp280_altitude = Pressure.readAltitude();
+  // Error check temperature readings.
+  String temp;
+  if ((ahtx0_temp.temperature > bmp280_temp && ahtx0_temp.temperature > bmp280_temp * 1.1) || (ahtx0_temp.temperature < bmp280_temp && ahtx0_temp.temperature * 1.1 < bmp280_temp)) {
+    temp = String("ERR");
+  } else {
+    // Take the average.
+    temp = String((bmp280_temp + ahtx0_temp.temperature) / 2);
   }
+  // Publish sensor data.
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.print(ahtx0_temp.temperature);
+  Serial.println(" C");
+  Serial.print(bmp280_temp);
+  Serial.println(" C");
+  Serial.print(ahtx0_humidity.relative_humidity);
+  Serial.println(" % rH");
+  Serial.print(bmp280_pressure);
+  Serial.println(" Pa");
+  Serial.print(bmp280_altitude);
+  Serial.println(" m");
 
-  outputPublish();
-  
   delay(LOOP_WAIT);
 }
 
